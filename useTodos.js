35833b37046e90b6c2d3321db564a8dbe25b860b
MatiasCasco <matiascasco_96@hotmail.com>
2025-02-19 16:33:49 -0300

    Primer commit se custom hooks

---
+++ useTodos.js
@@ -0,0 +1,58 @@
+import {useEffect, useReducer} from "react";
+import {todoReducer} from "../todoReducer.js";
+
+const initialState = [];
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodos = () => {
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() =>
+    {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
+
+    const handleNewTodo = (todo) => {
+        // console.log({ todo });
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo,
+        }
+
+        dispatch(action);
+
+    }
+
+    const handleDeleteTodo = (id) => {
+        // console.log({id});
+        const action = {
+            type: '[TODO] Remove Todo',
+            payload: id,
+        }
+
+        dispatch(action);
+    }
+
+    const handleToggleTodo = (id) => {
+        const action = {
+            type: '[TODO] Done Todo',
+            payload: id,
+        }
+
+        dispatch(action);
+    }
+
+    return {
+        todos,
+        todosCount: todos.length,
+        pedingTodosCount: todos.filter(todo => !todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+    }
+}
